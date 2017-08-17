e546c08d89f131bf6aae88da3200570768b7e547
Jiun Ting Wang <jiuntwang@gmail.com>
2017-08-17 08:41:23 -0700

    storage changes

--- app/scripts/playerStorage.js
+++ app/scripts/playerStorage.js
@@ -1,49 +1,33 @@
-function storageValue() {
-    var stored = $.mobile.pageContainer.pagecontainer("getActivePage").attr('id');
-    console.log("Your stored id is: " + stored);
-    
-    localStorage.setItem('stored', stored);
-    checkLocalStorage();
-    
-    
+var prefix = "playerStorage-";
+$("#save").click(function () { 
+    var key = $("#key").attr('value');
+    var value = $("#value").attr('value');
+    localStorage.setItem(prefix + key, value);      //******* setItem()
+    //localStorage[prefix+key] = value; also works
+    RewriteFromStorage();
+});
+
+function RewriteFromStorage() {
+    $("#data").empty();
+    for(var i = 0; i < localStorage.length; i++)    //******* length
+    {
+        var key = localStorage.key(i);              //******* key()
+        if(key.indexOf(prefix) == 0) {
+            var value = localStorage.getItem(key);  //******* getItem()
+            //var value = localStorage[key]; also works
+            var shortkey = key.replace(prefix, "");
+            $("#data").append(
+                $("<div class='kvp'>").html(shortkey + "=" + value)
+                   .append($("<input type='button' value='Delete'>")
+                           .attr('key', key)
+                           .click(function() {      //****** removeItem()
+                                localStorage.removeItem($(this).attr('key'));
+                                RewriteFromStorage();
+                            })
+                          )
+            );
+        }
+    }
 }
-var test = localStorage.getItem('stored');
-var resume = "#" + test;
-console.log("Your stored id is: " + resume);
 
-checkLocalStorage();
-
-
-// var prefix = "playerStorage-";
-// $("#save").click(function () { 
-//     var key = $("#key").attr('value');
-//     var value = $("#value").attr('value');
-//     localStorage.setItem(prefix + key, value);      //******* setItem()
-//     //localStorage[prefix+key] = value; also works
-//     RewriteFromStorage();
-// });
-
-// function RewriteFromStorage() {
-//     $("#data").empty();
-//     for(var i = 0; i < localStorage.length; i++)    //******* length
-//     {
-//         var key = localStorage.key(i);              //******* key()
-//         if(key.indexOf(prefix) == 0) {
-//             var value = localStorage.getItem(key);  //******* getItem()
-//             //var value = localStorage[key]; also works
-//             var shortkey = key.replace(prefix, "");
-//             $("#data").append(
-//                 $("<div class='kvp'>").html(shortkey + "=" + value)
-//                    .append($("<input type='button' value='Delete'>")
-//                            .attr('key', key)
-//                            .click(function() {      //****** removeItem()
-//                                 localStorage.removeItem($(this).attr('key'));
-//                                 RewriteFromStorage();
-//                             })
-//                           )
-//             );
-//         }
-//     }
-// }
-
-// RewriteFromStorage();
+RewriteFromStorage();
