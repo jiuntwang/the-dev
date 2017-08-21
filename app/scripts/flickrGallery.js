7d963d02afc456416a2a4f7c95258f81028f9ddd
Jiun Ting Wang <jiuntwang@gmail.com>
2017-08-20 21:26:11 -0700

    Added flickr API and Twitter

---
+++ app/scripts/flickrGallery.js
@@ -0,0 +1,29 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f47d127855c4746ff326f9fed6fc31ce&gallery_id=148106091-72157685425480983&format=json&nojsoncallback=1",
+  "method": "GET",
+  "headers": {}
+}
+
+$.ajax(settings).done(function (data) {
+  console.log(data);
+
+
+
+$("#galleryTitle").append(data.photos.photo[0].title + " Gallery");
+    	$.each( data.photos.photo, function( i, gp ) {
+
+var farmId = gp.farm;
+var serverId = gp.server;
+var id = gp.id;
+var secret = gp.secret;
+
+console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
+
+//  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+$("#flickr").append('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>');
+
+});
+});
--- app/scripts/playerStorage.js
+++ app/scripts/playerStorage.js
@@ -1,33 +1,3 @@
-// var prefix = "playerStorage-";
-// $("#save").click(function () { 
-//     var key = $("#key").attr('value');
-//     var value = $("#value").attr('value');
-//     localStorage.setItem(prefix + key, value);      //******* setItem()
-//     //localStorage[prefix+key] = value; also works
-//     RewriteFromStorage();
-// });
-
-// function RewriteFromStorage() {
-//     $("#data").empty();
-//     for(var i = 0; i < localStorage.length; i++)    //******* length
-//     {
-//         var key = localStorage.key(i);              //******* key()
-//         if(key.indexOf(prefix) == 0) {
-//             var value = localStorage.getItem(key);  //******* getItem()
-//             //var value = localStorage[key]; also works
-//             var shortkey = key.replace(prefix, "");
-//             $("#data").append(
-//                 $("<div class='kvp'>").html(shortkey + "=" + value)
-//                    .append($("<input type='button' value='Delete'>")
-//                            .attr('key', key)
-//                            .click(function() {      //****** removeItem()
-//                                 localStorage.removeItem($(this).attr('key'));
-//                                 RewriteFromStorage();
-//                             })
-//                           )
-//             );
-//         }
-//     }
-// }
-
-// RewriteFromStorage();
+var store = require('store')
+store.set('user', { name:'Marcus' })
+store.get('user').name == 'Marcus'
